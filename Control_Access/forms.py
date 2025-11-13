c7d1e63fea6ec611b5d5a7014749abd4e35716e2
Giovane da Silva Gobeti <giovane.gobeti@gmail.com>
2025-11-13 13:19:30 -0300

    Changing the User field to email.

--- Control_Access/forms.py
+++ Control_Access/forms.py
@@ -29,10 +29,14 @@ class FormularioCadastro(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super(FormularioCadastro, self).__init__(*args, **kwargs)
 
-        # Adicionando classes e placeholders para cada campo
-        self.fields['username'].widget.attrs.update(
-            {'class': 'form-control border-success', 'placeholder': 'Nome de Usuário'}
-            )
+        # 1. Esconde o campo 'username' padrão do UserCreationForm
+        self.fields['username'].widget = forms.HiddenInput()
+        # 2. Torna o 'username' não obrigatório (já que vamos usar o email como username)
+        self.fields['username'].required = False
+
+        
+        # Estilizando os campos que o usuário vai ver no formulário
+        # Adicionando classes e placeholders para cada campo do formulário
         self.fields['nome_completo'].widget.attrs.update(
             {'class': 'form-control border-success', 'placeholder': 'Insira seu nome completo'}
         )
@@ -52,18 +56,20 @@ class FormularioCadastro(UserCreationForm):
         
     class Meta(UserCreationForm.Meta):
         model = User
-        fields = ('username', 'nome_completo', 'email')
+        fields = ('username', 'email')
     
     def save(self, commit=True):
         # Primeiro, salva o usuário com username e senha (criptografada)
         user = super(FormularioCadastro, self).save(commit=False)
 
         # Agora, pegue os dados extras do formulário limpo
+        user.username = self.cleaned_data['email']  # Usando o email como username
         user.email = self.cleaned_data['email']
 
         # o 'User' não têm 'nome_completo', mas tem 'first_name' e 'last_name'
-        user.first_name = self.nome_completo.split(' ')[0]  # Primeiro nome
-        user.last_name = ' '.join(self.nome_completo.split(' ')[1:])  #
+        nome_completo = self.cleaned_data['nome_completo']
+        user.first_name = nome_completo.split(' ')[0]  # Primeiro nome
+        user.last_name = ' '.join(nome_completo.split(' ')[1:])  #
 
         if commit:
             user.save()  # Salva o usuário no banco de dados
@@ -71,10 +77,10 @@ class FormularioCadastro(UserCreationForm):
         return user
     
 
-# Formulário 2: Para criar o Administrador
-class FormularioAdministrador(forms.ModelForm): 
+# Formulário 2: Para criar o perfil de usuário (cpf, data_nascimento) (com estilo bootstrap)
+class UsuarioForm(forms.ModelForm): 
     def __init__(self, *args, **kwargs):
-        super(FormularioAdministrador, self).__init__(*args, **kwargs)
+        super(UsuarioForm, self).__init__(*args, **kwargs)
         # Adicionando classes e placeholders para cada campo do formulário
         self.fields['cpf'].widget.attrs.update(
             {'class': 'form-control border-success', 'placeholder': '___.____.____-__'}
@@ -83,10 +89,12 @@ class FormularioAdministrador(forms.ModelForm):
         self.fields['data_nascimento'].widget.attrs.update(
             {'class': 'form-control border-success'}
         )
+        # (Adicione aqui os outros campos do seu modelo 'Usuario', 
+        # como 'telefone', 'rua', etc., se você os quiser no formulário de cadastro)
 
     class Meta:
         model = Usuario
-        fields = ['cpf', 'data_nascimento'] # Campos para o Administrador
+        fields = ['cpf', 'data_nascimento']
         labels = {
             'cpf': 'CPF',
             'data_nascimento': 'Data de Nascimento',
@@ -101,8 +109,12 @@ class FormularioAdministrador(forms.ModelForm):
 class FormularioLogin (AuthenticationForm):
     def __init__(self, *args, **kwargs):
         super(FormularioLogin, self).__init__(*args, **kwargs)
+        
+        # 1. Muda o label de "Username" para "E-mail"
+        self.fields['username'].label = 'E-mail'
+        # 2. Muda o placeholder e adiciona a classe bootstrap
         self.fields['username'].widget.attrs.update(
-            {'class': 'form-control', 'placeholder': 'Nome de Usuário'}
+            {'class': 'form-control', 'placeholder': 'seu@email.com'}
         )
         self.fields['password'].widget.attrs.update(
             {'class': 'form-control', 'placeholder': 'Senha'}
--- Control_Access/views.py
+++ Control_Access/views.py
@@ -1,74 +1,51 @@
 from django.shortcuts import render, redirect
 from .forms import FormularioCadastro # Importe o formulário de criação de usuário
-from .forms import FormularioAdministrador
+from .forms import UsuarioForm 
 from .forms import FormularioLogin
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages 
 # Importe o formulário de criação do administrador 
 from django.db import transaction # Importa transaction para garantir que ambos os formulários sejam salvos juntos
 
-# transaction.atomic garante que ambas as operações de salvamento sejam atômicas
-@transaction.atomic
+@transaction.atomic # Garante que ambas as operações de salvamento sejam atômicas
 def view_cadastro(request):
     if request.method == 'POST':
         # Se o formulário foi enviado (MÉTODO POST)
         user_form = FormularioCadastro(request.POST)
-        profile_form = FormularioAdministrador(request.POST)
+        profile_form = UsuarioForm(request.POST)
+        
+        # 1. VERIFICA SE OS DOIS FORMULÁRIOS SÃO VÁLIDOS
         if user_form.is_valid() and profile_form.is_valid():
-            # 1. Salve o primeiro formulário e obtenha o objeto User
+            # 2. Salve primeiro o user e obtenha o objeto User
+            
             # Método save já cuida de criptografar a senha
             user = user_form.save()
 
-
-            # 2. Salve o segundo formulário, mas não confirme ainda (commit=False)
+            # 3. CRIE O PERFIL "EM MEMÓRIA" SEM SALVAR AINDA"
             profile = profile_form.save(commit=False)
-            # 3. Associe o objeto User ao perfil de Administrador
+
+            # 4. CONECTA O PERFIL AO USUÁRIO CRIADO
             profile.user = user
-            # 4. Agora salve o perfil de Administrador no banco de dados
+
+            # 5. AGORA SALVE O PERFIL NO BANCO DE DADOS
             profile.save()
 
             # Loga o usuário automaticamente após o cadastro
-            login(request, user)
+            login(request, user) 
             messages.success(request, "Cadastro realizado com sucesso!")
             return redirect('menu_principal')  # Redireciona para a página inicial após o cadastro
         else:
             messages.error(request, 'Erro no formulário. Verifique os dados inseridos.')
 
     else:
-        # Se a página foi apenas acessada (MÉTODO GET)
+        # Se a página foi apenas acessada (MÉTODO GET), crie dois formulários vazios
         user_form = FormularioCadastro()
-        profile_form = FormularioAdministrador()
+        profile_form = UsuarioForm()
+        
     
-    # Renderiza o template, passando o formulário como contexto
+    # 6. ENVIE OS DOIS FORMULÁRIOS PARA O TEMPLATE
     context = {
         'user_form' : user_form,
-        'profile_form' : profile_form
+        'profile_form': profile_form
     }
-    return render(request, 'Control_Access/Cadastrar_Conta.html', context)
-
-def login_view(request):
-    if request.method == 'POST':
-        # Use o formulário customizado para autenticação
-        form = FormularioLogin(request, data=request.POST)
-
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-
-            # Autentica o usuário
-            user = authenticate(username=username, password=password)
-
-            if user is not None:
-                login(request, user)
-                messages.info(request, f'Bem-vindo, {user.first_name}.')
-                return redirect('menu_principal') # Redireciona para a página inicial após o login
-            else:
-                messages.error(request, 'Usuário ou senha inválidos.')
-        else:
-            messages.error(request, 'Usuário ou senha inválidos.')
-    else:
-        form = FormularioLogin()
-    
-    # Renderiza o template, passando o formulário como contexto
-    return render(request, 'Control_Access/login.html', {'form': form})
-        
+    return render(request, 'Control_Access/Cadastrar_Conta.html', context)      
